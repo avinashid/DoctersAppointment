0b032b3eb4d678469b6eb78f72cd951c52b9dd31
Avinash Kumar <avinashkumar532004@gmail.com>
2023-12-05 20:34:09 +0530

    added backend

--- backend/controllers/doctorsControllers.js
+++ backend/controllers/doctorsControllers.js
@@ -3,7 +3,7 @@ const pool = require("../config/db");
 const getDoctors = async (req, res) => {
   try {
     const doctors = await pool.query("select * from doctors_information");
-    res.status(200).json(doctors);
+    res.status(200).json(doctors.rows);
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ Message: "Something went wrong" });
@@ -11,21 +11,47 @@ const getDoctors = async (req, res) => {
 };
 
 const getDoctorsFees = async (req, res) => {
-  const doctor = "Dr. Manik Dalvi";
-  const fees = await pool.query(
-    "SELECT * FROM doctors_fees WHERE doctor_name = $1",
-    [doctor]
-  );
-  res.status(200).json(fees.rows);
+  const doctor = req.body.doctor;
+  if (!doctor) return res.status(402).json({ message: "Invalid Data" });
+  try {
+    const fees = await pool.query(
+      "SELECT * FROM doctors_fees WHERE doctor_name = $1",
+      [doctor]
+    );
+    res.status(200).json(fees.rows);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
 const getDoctorsSlot = async (req, res) => {
-  const doctor = "Dr. Manik Dalvi";
-  const slots = await pool.query(
-    "SELECT * FROM doctors_slots WHERE doctor_name = $1",
-    [doctor]
-  );
-  res.status(200).json(slots.rows);
+  const doctor = req.body.doctor;
+  if (!doctor) return res.status(402).json({ message: "Invalid Data" });
+  try {
+    const slots = await pool.query(
+      "SELECT * FROM doctors_slots WHERE doctor_name = $1",
+      [doctor]
+    );
+    res.status(200).json(slots.rows);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
-module.exports = { getDoctors, getDoctorsFees,getDoctorsSlot };
+const setDoctorSlot = async (req, res) => {
+  const { doctor, time, date, type } = req.body;
+  if (!doctor || !time || !date || !type)
+    return res.status(402).json({ message: "Invalid Data" });
+  try {
+    const slots = await pool.query(
+      "insert into doctors_slots where docter_name = $1  (doctor_name,time,date,type) values ($2,$3,$4,$5) Returning *",
+      [doctor, time, date, type]
+    );
+    res.status(201).json(slots.rows);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+module.exports = { getDoctors, getDoctorsFees, getDoctorsSlot, setDoctorSlot };
--- backend/routes/doctorsRoutes.js
+++ backend/routes/doctorsRoutes.js
@@ -1,10 +1,16 @@
 const { Router } = require("express");
-const { getDoctors, getDoctorsFees, getDoctorsSlot } = require("../controllers/doctorsControllers");
+const {
+  getDoctors,
+  getDoctorsFees,
+  getDoctorsSlot,
+  setDoctorSlot,
+} = require("../controllers/doctorsControllers");
 
 const router = Router();
 
 router.get("/", getDoctors);
-router.get("/fees",getDoctorsFees)
-router.get("/slots",getDoctorsSlot)
+router.get("/fees", getDoctorsFees);
+router.get("/slots", getDoctorsSlot);
+router.post("/slots", setDoctorSlot);
 
 module.exports = router;
